0364db8d356d41e3be3de7f23674f00ff1d55dfb
Ilyos-py <abdulbosidjonbek@yahoo.mail>
2026-01-24 20:12:39 +0500

    Day 1 - Variable types and type() function

---
+++ day_1.py
@@ -0,0 +1,32 @@
+#All variable types(10)
+
+my_integer = 10 #only whole numbers without decimals
+print(type(my_integer)) # output: <class: int>
+
+my_float = 5.5 #numbers with decimals like 1.5 0.54
+print(type(my_float)) #output: <class: float>
+
+my_string = "Hello !" #sequence of charcters enclosed with single or double quotation marks
+print(type(my_string))#output: <class: str>
+
+my_boolean = True # True or False variable
+print(type(my_boolean))#output: <class: bool>
+
+my_set = {1, 5, 10, 3} # unordered collection of unique elements
+print(type(my_set))#output: <class: set>
+
+my_dictionary = {'key1': "value", 'key2': 1, 'key3': True}# key-value pairs enclosed with curly brackets
+print(type(my_dictionary))#output: <class: dict>
+
+my_tuple = (1, 2, 6, 12, 14) # ordered immutable collection enclosed with brackets
+print(type(my_tuple))#output: <class: tuple>
+
+my_range = range(10) #a sequence of numbers often used in loops
+print(type(my_range))#output: <class: range>
+
+my_list = ['Elijah', 18, True] # ordered collection of elements that support different data types
+print(type(my_list))#output: <class: list>
+
+my_none = None # Special value that represents the absence of value
+print(type(my_none))#output: <class: NoneType>
+
